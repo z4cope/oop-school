af569304d2d3ecc316a1bb8644f350761fef87e9
z4cope <63551733+z4cope@users.noreply.github.com>
2023-02-01 17:15:51 +0200

    Fix linters errors

--- student.rb
+++ student.rb
@@ -1,9 +1,9 @@
-require "./person"
+require './person'
 
 class Student < Person
   def initialize(classroom, name)
     super(15, name)
-    @classroom
+    @classroom = classroom
   end
 
   def play_hooky
