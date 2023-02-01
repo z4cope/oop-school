5a8aff8472743bdf7f1e7c38f71d804572227cbd
z4cope <63551733+z4cope@users.noreply.github.com>
2023-02-01 17:11:12 +0200

    Fix linters errors

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 require "./person"
 
 class Student < Person
-    def initialize(classroom, name)
-        super(15, name)
-        @classroom
-    end
+  def initialize(classroom, name)
+    super(15, name)
+    @classroom
+  end
 
-    def play_hooky
-        '¯\(ツ)/¯'
-    end
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
 require "./person"
 
 class Teacher < Person
-    def initialize(specialization, name)
-        super(40, name)
-        @specialization = specialization
-    end
+  def initialize(specialization, name)
+    super(40, name)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
